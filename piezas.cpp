feaf7ef5ffdddd1e715118ef42d30734ae9f5e3c
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 01:36:02 -0600

    Update piezas.cpp

---
+++ piezas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class piezasLegos{
+
+    private:
+        std::string nombre;
+
+    public:
+        void crearPieza(std::string nombre){
+            this ->nombre=nombre;
+    }
+
+
+};
