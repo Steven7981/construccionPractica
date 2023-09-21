4d4f0de1041b66f01b1b8cac31c5f77506b4ed9d
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 15:51:17 -0600

    Update partesCasa.cpp

--- partesCasa.cpp
+++ partesCasa.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include "piezas.cpp"
+#include <string>
 
 class partesCasa{
     private:
-        std::string pieza1;
-        std::string pieza2;
-        std::string pieza3;
-        std::string pieza4;
+        piezasLegos pieza1;
+        piezasLegos pieza2;
+        piezasLegos pieza3;
+        piezasLegos pieza4;
+        piezasLegos arregloPiezas[3];
     
     public:
 
-        std::string construirParte(std::string pieza1, std::string pieza2, std::string pieza3,std::string pieza4){
+        void construirParte(piezasLegos pieza1, piezasLegos pieza2, piezasLegos pieza3,piezasLegos pieza4){
+            arregloPiezas[0]=pieza1;
+            arregloPiezas[1]=pieza2;
+            arregloPiezas[2]=pieza3;
+            arregloPiezas[3]=pieza4;
             this->pieza1=pieza1;
             this->pieza2=pieza2;
             this->pieza3=pieza3;
             this->pieza4=pieza4;
         }
 
+        piezasLegos getPiezas(){
+            piezasLegos conjunto[3];
+            conjunto[0]=pieza1;
+            conjunto[1]=pieza2;
+            conjunto[2]=pieza3;
+            conjunto[3]=pieza4;
+            }
+        
 };
