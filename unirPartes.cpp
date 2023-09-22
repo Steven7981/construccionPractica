2e09ba8054b0470a5a8f69800acaaedb6eabe592
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 22:45:29 -0600

    Update unirPartes.cpp

--- unirPartes.cpp
+++ unirPartes.cpp
@@ -1,17 +1,22 @@
+#ifndef UNIR_CPP
+#define UNIR_CPP
 #include "partesCasa.cpp"
 #include <vector>
 
 class Conjunto{
     public:
-    std::queue<std::vector<std::string>> colaCasa;
+
     std::vector<std::string> listaPartes;
 
-    void unir(std::queue<std::string>& colaPiezas,int i){
+    std::queue<std::vector<std::string > > unir(std::queue<std::string>& colaPiezas,int i,std::queue<std::vector<std::string > > colaCasa){
         for (int j = 0; j<i;j++){
                 std::string aux=colaPiezas.front();
                 colaPiezas.pop();
                 listaPartes.push_back(aux);
             }
             colaCasa.push(listaPartes);
+            return colaCasa;
         }
 };
+
+#endif
