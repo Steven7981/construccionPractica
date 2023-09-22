8be08b3c458a3a1edd2d9363e0910a0dc0b78295
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 22:45:26 -0600

    Update partesCasa.cpp

--- partesCasa.cpp
+++ partesCasa.cpp
@@ -1,27 +1,25 @@
+#ifndef PARTES
+#define PARTES
 #include <iostream>
 #include "piezas.cpp"
 #include <string>
 #include <queue>
 
-class partesCasa{
-    private:
-        piezasLegos pieza1;
-        piezasLegos pieza2;
-        piezasLegos pieza3;
-        piezasLegos pieza4;
-        piezasLegos arregloPiezas[3];
-    
+class partesCasa{    
     public:
-        std::queue<std::vector<std::string>> colaPartes;
         std::vector<std::string> listaPiezas;
 
-        void construirParte(std::queue<std::string>& colaPiezas,int i){
+        std::queue<std::vector<std::string > > construirParte(std::queue<std::string>& colaPiezas,int i,std::queue<std::vector<std::string > > colaPartes){
             for (int j = 0; j<i;j++){
                 std::string aux=colaPiezas.front();
                 colaPiezas.pop();
                 listaPiezas.push_back(aux);
             }
             colaPartes.push(listaPiezas);
+            return colaPartes;
         }
 
+
 };
+
+#endif
