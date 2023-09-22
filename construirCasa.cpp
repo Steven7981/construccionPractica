0dfa184e56db1be58f2339e29d26971ba857e974
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 22:45:22 -0600

    Update construirCasa.cpp

--- construirCasa.cpp
+++ construirCasa.cpp
@@ -2,29 +2,43 @@
 #include <iostream>
 #include "partesCasa.cpp"
 #include <vector>
-#include "unirPartes.cpp"
 #include "piezas.cpp"
 #include <queue>
+#include "unirPartes.cpp"
 
 int main(){
     Conjunto arregloCasa;
+
     piezasLegos p1;
     piezasLegos p2;
     piezasLegos p3;
     piezasLegos p4;
-/*
-    std::queue <piezasLegos> colaPiezas;
-    p1.crearPieza("Bloque");
-    p2.crearPieza("piso");
-    p3.crearPieza("techo");
-    p4.crearPieza("ventana");
+    std::queue<std::vector<std::string > > colaCasa;
+    std::queue <std::string> colaPiezas;
 
     partesCasa cuarto;
-    partesCasa arreglo[999];
+    p1.crearPieza("Bloque",colaPiezas);
+    p2.crearPieza("piso",colaPiezas);
+    p3.crearPieza("techo",colaPiezas);
+    p4.crearPieza("ventana",colaPiezas);
+    cuarto.construirParte(colaPiezas,4,colaCasa);
 
-    cuarto.construirParte(p1,p2,p3,p4);
+    
+    while (!colaPiezas.empty()) {
+        std::cout << "Nombre de la pieza: " << colaPiezas.front() << std::endl;
+        colaPiezas.pop();
+    }
 
-    arregloCasa.unir(cuarto);
-*/
+    while (!colaCasa.empty()) {
+        std::vector<std::string> parte = colaCasa.front();
+        colaCasa.pop();
+
+        std::cout << "Parte: ";
+        for (const std::string& pieza : parte) {
+            std::cout << pieza << " ";
+        }
+        std::cout << std::endl;
+    }
     return 1;
+
 }
