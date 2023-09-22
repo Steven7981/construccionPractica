cd74d101e0e3f3a1fe8948a3a884e61e056058d7
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 23:06:16 -0600

    Update construirCasa.cpp

--- construirCasa.cpp
+++ construirCasa.cpp
@@ -8,13 +8,13 @@
 
 int main(){
     Conjunto arregloCasa;
-
     piezasLegos p1;
     piezasLegos p2;
     piezasLegos p3;
     piezasLegos p4;
     std::queue<std::vector<std::string > > colaCasa;
     std::queue <std::string> colaPiezas;
+    std::queue<std::vector<std::string > > colaFinal;
 
     partesCasa cuarto;
     p1.crearPieza("Bloque",colaPiezas);
@@ -22,23 +22,8 @@ int main(){
     p3.crearPieza("techo",colaPiezas);
     p4.crearPieza("ventana",colaPiezas);
     cuarto.construirParte(colaPiezas,4,colaCasa);
+    arregloCasa.unir(colaCasa,1,colaFinal);
 
-    
-    while (!colaPiezas.empty()) {
-        std::cout << "Nombre de la pieza: " << colaPiezas.front() << std::endl;
-        colaPiezas.pop();
-    }
-
-    while (!colaCasa.empty()) {
-        std::vector<std::string> parte = colaCasa.front();
-        colaCasa.pop();
-
-        std::cout << "Parte: ";
-        for (const std::string& pieza : parte) {
-            std::cout << pieza << " ";
-        }
-        std::cout << std::endl;
-    }
     return 1;
 
 }
