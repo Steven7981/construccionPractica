d2609cdad4bff760e791e53f381e5529baea6bb2
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 21:29:27 -0600

    Update construirCasa.cpp

--- construirCasa.cpp
+++ construirCasa.cpp
@@ -1,13 +1,19 @@
 
 #include <iostream>
 #include "partesCasa.cpp"
+#include <vector>
+#include "unirPartes.cpp"
+#include "piezas.cpp"
+#include <queue>
 
 int main(){
+    Conjunto arregloCasa;
     piezasLegos p1;
     piezasLegos p2;
     piezasLegos p3;
     piezasLegos p4;
-
+/*
+    std::queue <piezasLegos> colaPiezas;
     p1.crearPieza("Bloque");
     p2.crearPieza("piso");
     p3.crearPieza("techo");
@@ -18,5 +24,7 @@ int main(){
 
     cuarto.construirParte(p1,p2,p3,p4);
 
+    arregloCasa.unir(cuarto);
+*/
     return 1;
 }
