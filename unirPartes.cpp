5308837feaea86532a4c9de1131c94e890230cff
Steven7981 <stevenfengwu79@gmail.com>
2023-09-21 21:29:36 -0600

    Update unirPartes.cpp

--- unirPartes.cpp
+++ unirPartes.cpp
@@ -3,10 +3,15 @@
 
 class Conjunto{
     public:
-    std::vector<partesCasa> arregloConjunto;
+    std::queue<std::vector<std::string>> colaCasa;
+    std::vector<std::string> listaPartes;
 
-    void unir(partesCasa parte){
-        arregloConjunto.push_back(parte);
-    }
-
+    void unir(std::queue<std::string>& colaPiezas,int i){
+        for (int j = 0; j<i;j++){
+                std::string aux=colaPiezas.front();
+                colaPiezas.pop();
+                listaPartes.push_back(aux);
+            }
+            colaCasa.push(listaPartes);
+        }
 };
